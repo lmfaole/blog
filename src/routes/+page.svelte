571d52d409492dafe6fc51fb5f02c620452f4b27
OJ <olejorgenbakken@gmail.com>
2025-06-03 19:17:20 +0200

    svelte?

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<header class="site-header">
+    <a class="name" href="/index.html">Hjem</a>
+</header>
+<main>
+    <h1>Velkommen</h1>
+
+    <p>
+        <mark>Denne siden skrives om til Svelte i skrivende stund.</mark>
+    </p>
+
+    <p>
+        Jeg jobber som frontend-utvikler hos
+        <a href="https://fremtind.no">Fremtind Forsikring</a>, og prøver å ha
+        fokus på universell utforming, CSS og web-standarder.
+    </p>
+
+    <p>Du kan følge meg, stalke meg eller kontakte meg på
+        <a href="https://bsky.app/profile/lmfaole.no" rel="me">Bluesky</a>,
+        <a href="https://github.com/lmfaole" rel="me">Github</a> eller
+        <a href="https://www.linkedin.com/in/olejorgenbakken/" rel="me opener">LinkedIn</a>.
+    </p>
+</main>
