b0df31bfdc24aef9f9d9286de4007af3299223da
lmfaole <olejorgenbakken@gmail.com>
2025-06-03 22:39:16 +0200

    litt konvensjoner for svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+const { children } = $props();
+</script>
+
+<header>
+    Ole Jørgen Bakken
+</header>
+<main>
+    {@render children()}
+</main>
+<footer>
+    <p>Du kan følge, stalke eller kontakte meg på
+        <a href="https://bsky.app/profile/lmfaole.no" rel="me">Bluesky</a>,
+        <a href="https://github.com/lmfaole" rel="me">Github</a> eller
+        <a href="https://www.linkedin.com/in/olejorgenbakken/" rel="me">LinkedIn</a>.
+    </p>
+</footer>
+
+<style>
+    header, main, footer {
+        max-width: 90ch;
+        margin-inline: auto;
+    }
+
+    header {
+        padding-block: 1lh;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
-<header class="site-header">
-    <a class="name" href="/index.html">Hjem</a>
-</header>
-<main>
+
     <h1>Velkommen til en versjon 2</h1>
 
     <p>
@@ -14,9 +11,3 @@
         fokus på universell utforming, CSS og web-standarder.
     </p>
 
-    <p>Du kan følge meg, stalke meg eller kontakte meg på
-        <a href="https://bsky.app/profile/lmfaole.no" rel="me">Bluesky</a>,
-        <a href="https://github.com/lmfaole" rel="me">Github</a> eller
-        <a href="https://www.linkedin.com/in/olejorgenbakken/" rel="me opener">LinkedIn</a>.
-    </p>
-</main>
